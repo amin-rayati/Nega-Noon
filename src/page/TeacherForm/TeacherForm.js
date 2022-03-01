da0a84556fdde83e8cbefa230b4384e4132210bb
AMIN <aminrayatitooranposhti2000@gmail.com>
2022-03-02 00:44:31 +0330

    first commit

--- src/page/TeacherForm/TeacherForm.js
+++ src/page/TeacherForm/TeacherForm.js
@@ -16,12 +16,7 @@ const AskDore = () => {
       setShowState(false)
     }
   }
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/amin-rayati/NewEverset.git
-      git push -u origin main
+
   const [Loading, setLoading] = useState(false)
   const [imageUrl, setImageUrl] = useState()
   const [profileImg, setProfileImg] = useState([])
